7d729042e20c7728ebbb4470236d4a9974afeceb
Ccman11111 <154089596+Ccman11111@users.noreply.github.com>
2024-02-16 22:41:44 -0600

    Update readMe.lua

--- readMe.lua
+++ readMe.lua
@@ -1,1 +1,4 @@
---D&R is an easly customizeable way to send webhook messages to a Discord server from Roblox. For more info, use these links: https://discord.com/developers/docs/resources/webhook and https://create.roblox.com/docs/en-us/reference/engine/classes/HttpService
+--D&R is an easily customizable way to send webhook messages to a Discord server from Roblox. For more information, you can refer to the following links:
+
+--Discord Webhook Documentation: This resource provides details on how to work with webhooks in Discord. You can find it here https://discord.com/developers/docs/resources/webhook.
+--Roblox HttpService Documentation: If you’re interested in integrating Roblox with web services, the HttpService class documentation is a great place to start. You can explore it here https://create.roblox.com/docs/en-us/reference/engine/classes/HttpService.
